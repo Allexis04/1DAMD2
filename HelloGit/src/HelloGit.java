4713b9200574de3ffd8f248ed81154c8cb594e30
Diurno <Diurno@C8PC02>
2018-04-19 08:59:03 +0200

    the best was here

--- HelloGit/src/HelloGit.java
+++ HelloGit/src/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git World");
 		System.out.println("Hello Git World version 2");
+		System.out.println("Aqui estuvo Axel");
 	}
 }
